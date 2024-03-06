257e0824c13684d6607f5aec6de8f4d3b71d84ab
saurabh <saurabhkumar0184@gmail.com>
2024-03-06 23:31:28 +0530

    define schemas (Instructor, Course, Lead, Comment)

--- src/index.js
+++ src/index.js
@@ -1,20 +1,5 @@
 import dotenv from "dotenv"
-import dbConnect from "./db/index.js";
-import { app } from "./app.js";
+import express from "express"
 
 dotenv.config({path: './.env'})
 
-dbConnect()
-.then(
-    app.listen(process.env.PORT || 4000, () => {
-        console.log(`Server is started and running at port : ${process.env.PORT}`)
-    })
-)
-.catch((error) => {
-    console.log("Error ecountered while connecting : ", error)
-})
-
-app.on("error" , (error) => {
-    console.log("Error ecountered", error)
-    throw error
-})

fd91bed5932b1b43041108fd7c2e6d87111ab55e
saurabh <saurabhkumar0184@gmail.com>
2024-03-06 19:21:12 +0530

    configure index.js with db connection, initialize app.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
-import express from "express";
+import dotenv from "dotenv"
+import dbConnect from "./db/index.js";
+import { app } from "./app.js";
 
+dotenv.config({path: './.env'})
+
+dbConnect()
+.then(
+    app.listen(process.env.PORT || 4000, () => {
+        console.log(`Server is started and running at port : ${process.env.PORT}`)
+    })
+)
+.catch((error) => {
+    console.log("Error ecountered while connecting : ", error)
+})
+
+app.on("error" , (error) => {
+    console.log("Error ecountered", error)
+    throw error
+})

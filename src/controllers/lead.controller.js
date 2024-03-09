e375d352c67140850dfc855be7f821647fbaa0c8
saurabh <saurabhkumar0184@gmail.com>
2024-03-10 02:48:02 +0530

    add endpoints : updateLeadStatus, getLeadDetails, getAllLeadsForCourse

--- src/controllers/lead.controller.js
+++ src/controllers/lead.controller.js
@@ -46,4 +46,105 @@ const registerForCourse = asyncHandler(async (req, res) => {
 });
 
 
-export {registerForCourse,}
+const updateLeadStatus = asyncHandler(async (req, res) => {
+    const { leadId } = req.params;
+    const { status } = req.body;
+
+    if (!status) {
+        throw new ApiError(400, "Status is required");
+    }
+
+    const lead = await prisma.lead.findUnique({
+        where: {
+            id: leadId
+        }
+    });
+
+    if (!lead) {
+        throw new ApiError(404, "Lead not found");
+    }
+
+    const updatedLead = await prisma.lead.update({
+        where: {
+            id: leadId
+        },
+        data: {
+            status: status.toUpperCase()
+        }
+    });
+
+    return res.status(200)
+    .json(
+        new ApiResponse(200, updatedLead, "Lead status updated successfully")
+    );
+});
+
+
+const getLeadDetails = asyncHandler(async (req, res) => {
+    const { name, email } = req.query;
+
+    if(!name && !email) {
+        throw new ApiError(400, "One of the field is required")
+    }
+
+    const searchConditions = {};
+    if (name) {
+        searchConditions.name = { contains: name };
+    }
+    if (email) {
+        searchConditions.email = { contains: email };
+    }
+
+    const leads = await prisma.lead.findMany({
+        where: searchConditions,
+        select: {
+            id : true,
+            name: true,
+            email: true,
+            phoneNumber: true,
+            linkedInProfile: true,
+            courses: true,
+            comments: true
+        }
+    });
+
+    return res.status(200).
+    json(
+        new ApiResponse(200, leads, "Leads retrieved successfully")
+    );
+});
+
+
+const getAllLeadsForCourse = asyncHandler(async (req, res) => {
+    const { courseId } = req.params;
+
+    const leads = await prisma.lead.findMany({
+        where: {
+            courses: {
+                some: {
+                    id: courseId
+                }
+            }
+        },
+        select: {
+            id: true,
+            name: true,
+            email: true,
+            phoneNumber: true,
+            linkedInProfile: true,
+            status: true,
+            comments: true 
+        }
+    });
+
+    return res.status(200)
+    .json(
+        new ApiResponse(200, leads, "Leads retrieved successfully")
+    );
+});
+
+
+export {registerForCourse,
+        updateLeadStatus,
+        getLeadDetails,
+        getAllLeadsForCourse }
--- src/routes/lead.routes.js
+++ src/routes/lead.routes.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { registerForCourse } from "../controllers/lead.controller.js";
+import { registerForCourse, getLeadDetails, updateLeadStatus, getAllLeadsForCourse } from "../controllers/lead.controller.js";
 
 const router = new Router();
 
 router.route("/register/:courseId").post(registerForCourse);
+router.route("/update/:leadId").patch(updateLeadStatus);
+router.route("/lead-details").get(getLeadDetails);
+router.route("/leads-of-course/:courseId").get(getAllLeadsForCourse)
 
 export default router;
